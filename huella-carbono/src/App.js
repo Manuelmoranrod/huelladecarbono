9689b7137fc567bdefacaaad311dfb0ea2430f66
Iván <ivanqgervas@gmail.com>
2021-10-18 17:20:35 +0200

    finish first part

--- huella-carbono/src/App.js
+++ huella-carbono/src/App.js
@@ -1,19 +1,27 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import Header from './components/Header/Header';
 import Main from './components/Main/Main';
 import Footer from './components/Footer/Footer';
 import './styles/styles.scss'
 
+// Context
+import userContext from './context/userContext'
 
 function App() {
+
+  // States
+  const [user, setUser] = useState(null)
+
   return (
     <div>
-      <BrowserRouter>
-        <Header />
-        <Main />
-      </BrowserRouter>
-      <Footer />
+      <userContext.Provider value={{user, setUser}}>
+        <BrowserRouter>
+          <Header />
+          <Main />
+        </BrowserRouter>
+        <Footer />
+      </userContext.Provider>
     </div>
   );
 }
--- huella-carbono/src/components/Login/Login.jsx
+++ huella-carbono/src/components/Login/Login.jsx
@@ -1,11 +1,44 @@
-import React from "react";
+import axios from "axios";
+import React, { useContext } from "react";
+
+import GoogleLogin, { GoogleLogout } from 'react-google-login';
+
+// Context
+import userContext from "../../context/userContext";
+
+import login from '../../login'
 
 const Login = () => {
+
+  // Context
+  const { user, setUser } = useContext(userContext)
+
+  const handleSubmit = async (event) => {
+    event.preventDefault()
+
+    const email = event.target.email.value
+    const password = event.target.password.value
+
+    const { data } = await axios.post('http://localhost:3001/auth/login', {
+      email,
+      password
+    })
+
+    console.log('data', data);
+  }
+
+  const loginGoogle = (resGoogle) => {
+    console.log(resGoogle);
+  }
+
+  const logoutSucces = (respuesta) => {
+    console.log('desconectadp');
+  }
+
   return (
-    <form className="form-login" method="POST"
-    action="http://localhost:3001/login">
+    <form className="form-login" onSubmit={handleSubmit}>
       <label>
-        <p>Username</p>
+        <p>Email</p>
         <input type="text" name="email" />
       </label>
       <label>
@@ -15,8 +48,24 @@ const Login = () => {
       <div>
         <button type="submit">Submit</button>
       </div>
+      <GoogleLogin
+        clientId="302295141577-ro76q0pd5jfle7v6hh6q7on7b1j017sl.apps.googleusercontent.com"
+        buttonText="Iniciar sesión con Google"
+        onSuccess={loginGoogle}
+        onFailure={loginGoogle}
+        cookiePolicy={'single_host_origin'}
+      />
     </form>
   );
 };
 
 export default Login;
+
+
+{/* <GoogleLogin
+  clientId="302295141577-ro76q0pd5jfle7v6hh6q7on7b1j017sl.apps.googleusercontent.com"
+  buttonText="Iniciar sesión con Google"
+  onSuccess={loginGoogle}
+  onFailure={loginGoogle}
+  cookiePolicy={'single_host_origin'}
+/> */}
