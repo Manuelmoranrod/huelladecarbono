9f534fba45d88e3d0b57315f4d3ef1135e6a5830
Manuel Moran <manuel9423@hotmail.com>
2021-10-23 11:27:35 +0200

    clear code

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,5 +1,4 @@
 const router = require('express').Router()
-const passport = require('passport')
 const authControllers = require('../controllers/auth.controllers')
 
 router.post('/register', authControllers.postRegister)
