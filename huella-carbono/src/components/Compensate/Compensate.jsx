f14ea60b710f6a3f88b61e6f26dbc9e57b2908a7
Manuel Moran <manuel9423@hotmail.com>
2021-10-24 18:17:08 +0200

    compensate

--- huella-carbono/src/components/Compensate/Compensate.jsx
+++ huella-carbono/src/components/Compensate/Compensate.jsx
@@ -1,7 +1,49 @@
 import React from "react";
 
 const Compensate = () => {
-  return <div>Compensate</div>;
+  return (
+    <div className="container">
+      <div className="cabecera">
+        <h1>Planes</h1>
+        <p>Breve apartado sobre los tipos de planes que hay bla bla bla</p>
+      </div>
+      <div className="plan-card">
+        <div className="img">
+          Imagen
+        </div>
+        <h2>Premium</h2>
+        <p>Breve comentario</p>
+        <button className="slct">Seleccionar</button>
+      </div>
+
+      <div className="plan-card">
+        <div className="img">
+          Imagen
+        </div>
+        <h2>High</h2>
+        <p>Breve comentario</p>
+        <button className="slct">Seleccionar</button>
+      </div>
+
+      <div className="plan-card">
+        <div className="img">
+          Imagen
+        </div>
+        <h2>Mid</h2>
+        <p>Breve comentario</p>
+        <button className="slct">Seleccionar</button>
+      </div>
+
+      <div className="plan-card">
+        <div className="img">
+          Imagen
+        </div>
+        <h2>Low</h2>
+        <p>Breve comentario</p>
+        <button className="slct">Seleccionar</button>
+      </div>
+    </div>
+  );
 };
 
 export default Compensate;
