e9cd7f71fa2ea607d137f54e15adfca8b3722002
Manuel Moran <manuel9423@hotmail.com>
2021-10-27 16:46:25 +0200

    carousel

--- huella-carbono/src/components/Carousel/Carousel.jsx
+++ huella-carbono/src/components/Carousel/Carousel.jsx
@@ -1,52 +1,43 @@
 import React from "react";
-import one from "../../assets/caru-first.svg";
-import two from "../../assets/c-dos.svg";
-import tres from "../../assets/c-tres.svg";
-import Carousel from 'react-bootstrap/Carousel';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
+import { Link } from "react-router-dom";
+
+// Vistas del OnBoard de la App
+import one from "../../assets/c-one-letra.svg";
+import two from "../../assets/c-two-letra.svg";
+import three from "../../assets/c-three-letra.svg";
 
 
-const PhotoCarousel = () => {
-
+const FirstCarousel = () => {
 
   return (
-    <Carousel>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src={one}
-          alt="First slide"
-        />
-        <Carousel.Caption>
-          <h3>Mide tu huella de carbono</h3>
-          <p>Calcula y realiza un seguimiento preciso de tu impacto en el planeta</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src={two}
-          alt="Second slide"
-        />
-
-        <Carousel.Caption>
-          <h3>Compensa tus emisiones</h3>
-          <p>Apoya iniciativas y causas sostenibles para compensar tu huella de carbono</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src={tres}
-          alt="Third slide"
-        />
-        <Carousel.Caption>
-          <h3>Lidera el ranking de tu ciudad</h3>
-          <p>Únete a una comunidad comprometida con el planeta dispuesta a tomar acción</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-    </Carousel>
+    <div className="carousel">
+      <Carousel
+        showArrows={false}
+        showThumbs={false}
+        showStatus={false}
+        autoPlay={false}
+        infiniteLoop={false}
+      >
+        <div>
+          <img src={one} />
+        </div>
+        <div>
+          <img src={two} />
+        </div>
+        <div>
+          <img src={three} />
+        </div>
+        <div>
+          <div>Logo app</div>
+          <p>Eslogan de la app</p>
+          <Link to="/register"><button>CREAR CUENTA</button></Link><br />
+          <Link to="/login"><button>LOGIN</button></Link>
+        </div>
+      </Carousel>
+    </div>
   );
 };
 
-export default PhotoCarousel;
+export default FirstCarousel;
